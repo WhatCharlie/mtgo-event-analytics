df1a7510ff36e08877fe50df8104ac007aeb9893
Charlie Whatley <charliewhatley0@gmail.com>
2026-07-09 17:44:31 -0400

    Adjusted the csv save method so that files are not overwritten on each use of the parser

--- scraper/file_io.py
+++ scraper/file_io.py
@@ -1,5 +1,7 @@
 import json
 from pathlib import Path
+import os
+import pandas as pd
 
 def save_raw_json(data, output_dir="../data/raw"):
     """
@@ -21,3 +23,23 @@ def save_raw_json(data, output_dir="../data/raw"):
 
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
+
+def update_csv(new_data, filepath, unique_column):
+    """
+    Append new data to an existing CSV and remove duplicates.
+    """
+
+    if os.path.exists(filepath):
+        old_data = pd.read_csv(filepath)
+        combined = pd.concat([old_data, new_data], ignore_index=True)
+    else:
+        combined = new_data
+
+    combined = combined.drop_duplicates(
+        subset=unique_column,
+        keep="last"
+    )
+
+    combined.to_csv(filepath, index=False)
+
+    return combined
